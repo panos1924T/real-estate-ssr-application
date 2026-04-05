6f0b37dca555b1072c58f08c54ef7f196ab6f123
pantsath <pants.ath@gmail.com>
2026-04-05 12:14:38 +0300

    feat: add enable jpa auditing for timestamps

--- src/main/java/pants_cf/real_estate/RealEstateApplication.java
+++ src/main/java/pants_cf/real_estate/RealEstateApplication.java
@@ -2,8 +2,10 @@ package pants_cf.real_estate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RealEstateApplication {
 
 	public static void main(String[] args) {
