058694bf750e9db831cd0bf0bfa9e9ef65889ef8
pantsath <pants.ath@gmail.com>
2026-04-05 11:05:24 +0300

    feat: initial commit

---
+++ src/test/java/pants_cf/real_estate/RealEstateApplicationTests.java
@@ -0,0 +1,13 @@
+package pants_cf.real_estate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
