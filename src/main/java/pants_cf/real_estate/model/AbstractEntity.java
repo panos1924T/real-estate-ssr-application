64a6cbae7880c00fb32bd51ac0abfcc23e31ed50
pantsath <pants.ath@gmail.com>
2026-04-05 12:22:01 +0300

    feat: initialize abstract entity use for timestamps

---
+++ src/main/java/pants_cf/real_estate/model/AbstractEntity.java
@@ -0,0 +1,32 @@
+package pants_cf.real_estate.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class AbstractEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "DATETIME")
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false, columnDefinition = "DATETIME")
+    private Instant updatedAt;
+
+}
